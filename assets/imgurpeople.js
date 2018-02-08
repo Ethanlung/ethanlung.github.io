0a761444e3bb7f0d1cd45812e75ade2ece2f0ed0
Andy Young <zeusandninja@gmail.com>
2018-02-08 12:44:43 -1000

    new imgur thing
    new imgur thing

---
+++ assets/imgurpeople.js
@@ -0,0 +1,28 @@
+var request_url = "https://api.imgur.com/3/album/HA0fP";
+function requestAlbum() {
+  var req = new XMLHttpRequest();
+  
+  req.onreadystatechange = function() { 
+     if (req.readyState == 4 && req.status == 200) {
+       processRequest(req.responseText);
+     } else {
+       console.log("Error with Imgur Request.");
+     }
+  }
+  req.open("GET", request_url, true); // true for asynchronous     
+  req.setRequestHeader("Authorization", "Client-ID 7fe141c366b6a38");
+  req.send(null);
+}
+function processRequest(response_text) {
+  if (response_text == "Not found") {
+    console.log("Imgur album not found.");
+  } else {
+    var json = JSON.parse(response_text);
+      for(var i = 0; i < json.data.images.length; i++) {
+          var imgurSRC = "https://imgur.com/" + json.data.images[i].id + ".jpg";
+          document.getElementById("works").innerHTML = document.getElementById("works").innerHTML + "<figure class='wowload fadeInUp lul'><img src='" + imgurSRC + "' alt='img01'/><figcaption><a href='" + imgurSRC + "' title='1' data-gallery></a></figcaption></figure>"
+          console.log(imgurSRC)
+      }
+  }
+}
+requestAlbum();
